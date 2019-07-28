3594b4bc2fefb91ee5fc3a5a3b32583d1bbe0143
zemunkh <zemunkh@gmail.com>
2019-07-28 22:15:41 +0800

    Update vanillaCalendar.js
    You can select a day in a week range.

--- src/vanillaCalendar.js
+++ src/vanillaCalendar.js
@@ -3,6 +3,9 @@ var vanillaCalendar = {
   next: document.querySelectorAll('[data-calendar-toggle="next"]')[0],
   previous: document.querySelectorAll('[data-calendar-toggle="previous"]')[0],
   label: document.querySelectorAll('[data-calendar-label="month"]')[0],
+
+  timetable: document.querySelectorAll('[data-timetable-area="time"]')[0],
+
   activeDates: null,
   date: new Date(),
   todaysDate: new Date(),
@@ -10,24 +13,55 @@ var vanillaCalendar = {
   init: function (options) {
     this.options = options
     this.date.setDate(1)
-    this.createMonth()
+    this.createWeek(0)
     this.createListeners()
   },
 
   createListeners: function () {
     var _this = this
+
+
     this.next.addEventListener('click', function () {
+      var weekNum = 0;
+      var day = _this.date.getDate()
+      _this.date.setDate(day)
+
+      console.log("First Day of the next week is: " + day + " Month is " + _this.date.getMonth())
+      var n = day/7
+      var dec_portion = Math.floor(n);
+      var rem_portion = n - Math.floor(n)
+      if(rem_portion === 0) {
+        var weekNum = dec_portion;
+        console.log("Week #" + weekNum)
+      } else {
+        var weekNum = dec_portion + 1;
+        console.log("Week #" + weekNum)
+      }
+
       _this.clearCalendar()
-      var nextMonth = _this.date.getMonth() + 1
-      _this.date.setMonth(nextMonth)
-      _this.createMonth()
+      _this.createWeek(weekNum)
     })
-    // Clears the calendar and shows the previous month
+    // Clears the calendar and shows the previous week
     this.previous.addEventListener('click', function () {
+      var weekNum = 0;
+      var day = _this.date.getDate() - 14
+      _this.date.setDate(day)
+
+      day = _this.date.getDate()
+      console.log("First Day of the last week is: " + day + " Month is " + _this.date.getMonth())
+      var n = day/7
+      var dec_portion = Math.floor(n);
+      var rem_portion = n - Math.floor(n)
+      if(rem_portion === 0) {
+        var weekNum = dec_portion;
+        console.log("Week #" + weekNum)
+      } else {
+        var weekNum = dec_portion + 1;
+        console.log("Week #" + weekNum)
+      }
+
       _this.clearCalendar()
-      var prevMonth = _this.date.getMonth() - 1
-      _this.date.setMonth(prevMonth)
-      _this.createMonth()
+      _this.createWeek(weekNum)
     })
   },
 
@@ -38,14 +72,6 @@ var vanillaCalendar = {
     newDay.className = 'vcal-date'
     newDay.setAttribute('data-calendar-date', this.date)
 
-    // if it's the first day of the month
-    if (num === 1) {
-      if (day === 0) {
-        newDay.style.marginLeft = (6 * 14.28) + '%'
-      } else {
-        newDay.style.marginLeft = ((day - 1) * 14.28) + '%'
-      }
-    }
 
     if (this.options.disablePastDays && this.date.getTime() <= this.todaysDate.getTime() - 1) {
       newDay.classList.add('vcal-date--disabled')
@@ -67,11 +93,15 @@ var vanillaCalendar = {
     this.activeDates = document.querySelectorAll(
       '[data-calendar-status="active"]'
     )
+
     for (var i = 0; i < this.activeDates.length; i++) {
       this.activeDates[i].addEventListener('click', function (event) {
         var picked = document.querySelectorAll(
           '[data-calendar-label="picked"]'
         )[0]
+
+        _this.createTime()
+
         picked.innerHTML = this.dataset.calendarDate
         _this.removeActiveClass()
         this.classList.add('vcal-date--selected')
@@ -79,23 +109,80 @@ var vanillaCalendar = {
     }
   },
 
-  createMonth: function () {
+  createWeek: function (weekNum) {
+    var week = 0
+    if (weekNum === 0) {
+      var day = this.todaysDate.getDate()
+      var n = day/7
+      var dec_portion = Math.floor(n);
+      var rem_portion = n - Math.floor(n)
+      if(rem_portion === 0) {
+        var week = dec_portion;
+        console.log("Week #" + week)
+      } else {
+        var week = dec_portion + 1;
+        console.log("Week #" + week)
+      }
+      if(this.todaysDate.getDay() === 0) {
+        this.date.setDate(day - 6)
+      } else {
+        this.date.setDate(day - this.todaysDate.getDay() + 1)
+      }
+      console.log("The first day of the week #" + this.date.getDate())
+      weekNum = week
+    } else {
+      var day = this.date.getDate()
+      var n = day/7
+      var dec_portion = Math.floor(n);
+      var rem_portion = n - Math.floor(n)
+      if(rem_portion === 0) {
+        var week = dec_portion;
+        console.log("Week #" + week)
+      } else {
+        var week = dec_portion + 1;
+        console.log("Week #" + week)
+      }
+    }
+
     var currentMonth = this.date.getMonth()
-    while (this.date.getMonth() === currentMonth) {
+    while (weekNum === week) {
       this.createDay(
         this.date.getDate(),
         this.date.getDay(),
-        this.date.getFullYear()
+        this.date.getFullYear(),
       )
-      this.date.setDate(this.date.getDate() + 1)
+      if (this.date.getDay() === 0) {
+        // this.date.setDate(7*week + 1)
+        this.date.setDate((this.date.getDate() + 1))
+        console.log("Week : " + week + " -- Days: " + (7*week + 1))
+        week++
+      } else {
+        this.date.setDate(this.date.getDate() + 1)
+      }
     }
-    // while loop trips over and day is at 30/31, bring it back
-    this.date.setDate(1)
-    this.date.setMonth(this.date.getMonth() - 1)
 
     this.label.innerHTML =
-      this.monthsAsString(this.date.getMonth()) + ' ' + this.date.getFullYear()
+      this.monthsAsString(this.date.getMonth()) + (this.date.getDate())
+      + ',  ' + this.weekdaysAsString(this.date.getDay())
+
     this.dateClicked()
+  },
+
+  monthsAsString: function (monthIndex) {
+    return [
+      'I-',
+      'II-',
+      'III-',
+      'IV-',
+      'V-',
+      'VI-',
+      'VII-',
+      'VIII-',
+      'IX-',
+      'X-',
+      'XI-',
+      'XII-'
+    ][monthIndex]
   },
 
   monthsAsString: function (monthIndex) {
